9cde0daa2cd73f5d5e9a2fdad7c102ecaecb45f6
Ryan Kirsch <agalloch88@gmail.com>
2022-03-21 07:25:26 -0400

    fix syntax errors

--- fireship/arrays.go
+++ fireship/arrays.go
@@ -5,7 +5,7 @@ import "fmt"
 func main4() {
 	var DeploymentOptions = [4]string{"R-pi", "AWS", "GCP", "Azure"}
 
-	for i := 0; i len(DeploymentOptions); i++ {
+	for i := 0; i < len(DeploymentOptions); i++ {
 		option := DeploymentOptions[i]
 		fmt.Println(option)
 	}
--- fireship/arrays_refactored.go
+++ fireship/arrays_refactored.go
@@ -5,7 +5,7 @@ import "fmt"
 func main5() {
 	DeploymentOptions := [...]string{"R-pi", "AWS", "GCP", "Azure"}
 
-	for, index, option := range DeploymentOptions {
+	for index, option := range DeploymentOptions {
 		fmt.Println(index, option)
 	}
 }
