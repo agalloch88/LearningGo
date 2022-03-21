4c00c9e7597c84e944e8401744197e18064b8b43
Ryan Kirsch <agalloch88@gmail.com>
2022-03-21 10:24:15 -0400

    functions and return types in go

--- cards/main.go
+++ cards/main.go
@@ -6,8 +6,17 @@ func main()  {
 	// var card string = "Ace of Spades"
 	// ^^ long-winded version of the below
 	// only use := on initialization of variable, not on reassignment
-	card := "Ace of Spades"
-	card = "Five of Diamonds"
-	
+	card := newCard()
+	// card = "Five of Diamonds"
+
 	fmt.Println(card)
+}
+// functions can be declared in the same file as the main function, but if it will return value, need to specifically say what type it returns
+
+func newCard() string {
+	return "Five of Diamonds"
+}
+
+func secondNewCard() int {
+	return 12
 }
