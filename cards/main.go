dad778d71dba13c1b08a2bc0d09a16ed30d5dc87
Ryan Kirsch <agalloch88@gmail.com>
2022-03-21 10:40:11 -0400

    slices and for loops

--- cards/main.go
+++ cards/main.go
@@ -6,10 +6,24 @@ func main()  {
 	// var card string = "Ace of Spades"
 	// ^^ long-winded version of the below
 	// only use := on initialization of variable, not on reassignment
-	card := newCard()
-	// card = "Five of Diamonds"
+	// card := newCard()
+	// // card = "Five of Diamonds"
 
-	fmt.Println(card)
+	// fmt.Println(card)
+
+	// how to declare a slice (aka dynamic array) and add new values to it
+	// append does not modify existing slice, but rather returns a new slice and reassigns the variable
+
+	cards := []string{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades")
+
+	// how to set up for loop in go
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+
+	fmt.Println(cards)
 }
 // functions can be declared in the same file as the main function, but if it will return value, need to specifically say what type it returns
 
